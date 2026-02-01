6fc9bf68db6e9a77947a47772ca823c9048619a0
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-01-31 16:03:13 -1000

    Initial commit on Project1 assignment

---
+++ Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,14 @@
+var sentence = "hello world from the variable sentence", 
+alertMessage = "Alert Pop Up", 
+syntaxSentence = "First sentence in the writeIn method\'s usage\\but this is the second", 
+superSentence = sentence + alertMessage;
+
+var num1 = 66;
+var num2 = 10;
+
+window.alert(alertMessage);
+//document.writeln(sentence);
+//document.writeln(syntaxSentence);
+//document.writeln(superSentence);
+
+document.writeln(num1 + num2);
